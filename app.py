2145e066cba37733e622c1b28fd921a8e86c64cb
Romain Boyrie <romain@boyrie.email>
2022-04-20 13:58:56 +0200

    Modern Times

--- app.py
+++ app.py
@@ -1,72 +1,34 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-import json
-import logging
 import sys
-import time
 import traceback
-import uuid
-
 from datetime import datetime
-
-from azure.cognitiveservices.language.luis.authoring import LUISAuthoringClient
-from azure.cognitiveservices.language.luis.authoring.models import ApplicationCreateObject
-from azure.cognitiveservices.language.luis.runtime import LUISRuntimeClient
-from msrest.authentication import CognitiveServicesCredentials
-from functools import reduce
+from http import HTTPStatus
 
 from aiohttp import web
 from aiohttp.web import Request, Response, json_response
 from botbuilder.core import (
-    BotFrameworkAdapterSettings,
-    TurnContext,
     BotFrameworkAdapter,
+    BotFrameworkAdapterSettings,
+    ConversationState,
+    MemoryStorage,
+    TurnContext,
+    UserState,
 )
 from botbuilder.core.integration import aiohttp_error_middleware
 from botbuilder.schema import Activity, ActivityTypes
 
-from bot import MyBot
+from bots import CustomPromptBot
 from config import DefaultConfig
-from opencensus.ext.azure.log_exporter import AzureLogHandler
-from opencensus.ext.azure.log_exporter import AzureEventHandler
-
-################################################################
-logger = logging.getLogger(__name__)
-#logger.addHandler(AzureEventHandler(connection_string='InstrumentationKey=<your_key>'))
-logger.setLevel(logging.INFO)
-logger.info('Hello, World!')
-################################################################
-
 
 CONFIG = DefaultConfig()
 
 # Create adapter.
 # See https://aka.ms/about-bot-adapter to learn more about how bots work.
 SETTINGS = BotFrameworkAdapterSettings(CONFIG.APP_ID, CONFIG.APP_PASSWORD)
-#print(CONFIG.APP_ID, CONFIG.APP_PASSWORD)
 ADAPTER = BotFrameworkAdapter(SETTINGS)
-# LUIS adapter
-authoringKey = CONFIG.LUIS_KEY
-authoringEndpoint = CONFIG.LUIS_ENDPOINT
-app_id = CONFIG.LUIS_ID
-client = LUISAuthoringClient(authoringEndpoint, CognitiveServicesCredentials(authoringKey))
-versionId = "0.1"
 
-# Define labeled example
-client.model.add_intent(app_id, versionId, 'inform')
-labeledExampleUtteranceWithMLEntity = {'text': 'I am looking for a honeymoon package to Manaus. I heard it is the perfect honeymooners paradise. The wedding cost us an arm and a leg so I really need something reasonable.',
- 'entityLabels': [{'startCharIndex': 40,
-   'endCharIndex': 45,
-   'entityName': 'dst_city'}],
- 'intentName': 'inform'}
-
-print("Labeled Example Utterance:", labeledExampleUtteranceWithMLEntity)
-
-# Add an example for the entity.
-# Enable nested children to allow using multiple models with the same name.
-# The quantity subentity and the phraselist could have the same exact name if this is set to True
-client.examples.add(app_id, versionId, labeledExampleUtteranceWithMLEntity, {"enableNestedChildren": True})
 
 # Catch-all for errors.
 async def on_error(context: TurnContext, error: Exception):
@@ -95,20 +57,30 @@ async def on_error(context: TurnContext, error: Exception):
         # Send a trace activity, which will be displayed in Bot Framework Emulator
         await context.send_activity(trace_activity)
 
+    # Clear out state
+    await CONVERSATION_STATE.delete(context)
 
+
+# Set the error handler on the Adapter.
+# In this case, we want an unbound method, so MethodType is not needed.
 ADAPTER.on_turn_error = on_error
 
-# Create the Bot
-BOT = MyBot()
+# Create MemoryStorage and state
+MEMORY = MemoryStorage()
+USER_STATE = UserState(MEMORY)
+CONVERSATION_STATE = ConversationState(MEMORY)
+
+# Create Bot
+BOT = CustomPromptBot(CONVERSATION_STATE, USER_STATE)
 
 
-# Listen for incoming requests on /api/messages
+# Listen for incoming requests on /api/messages.
 async def messages(req: Request) -> Response:
     # Main bot message handler.
     if "application/json" in req.headers["Content-Type"]:
         body = await req.json()
     else:
-        return Response(status=415)
+        return Response(status=HTTPStatus.UNSUPPORTED_MEDIA_TYPE)
 
     activity = Activity().deserialize(body)
     auth_header = req.headers["Authorization"] if "Authorization" in req.headers else ""
@@ -116,7 +88,7 @@ async def messages(req: Request) -> Response:
     response = await ADAPTER.process_activity(activity, auth_header, BOT.on_turn)
     if response:
         return json_response(data=response.body, status=response.status)
-    return Response(status=201)
+    return Response(status=HTTPStatus.OK)
 
 
 APP = web.Application(middlewares=[aiohttp_error_middleware])
