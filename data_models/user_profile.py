172e82eb7268c147036dd5c25be7c9663daa2d58
Romain Boyrie <romain@boyrie.email>
2022-04-21 09:07:48 +0200

    Adapt to travelers

--- data_models/user_profile.py
+++ data_models/user_profile.py
@@ -3,7 +3,12 @@
 
 
 class UserProfile:
-    def __init__(self, name: str = None, age: int = 0, date: str = None):
+    def __init__(self, name: str = None, or_city: str = None,
+                 dst_city: str = None, str_date: str = None,
+                 end_date: str = None, budget: int = 0):
         self.name = name
-        self.age = age
-        self.date = date
+        self.or_city = or_city
+        self.dst_city = dst_city
+        self.str_date = str_date
+        self.end_date = end_date
+        self.budget = budget
