498af7da7353e0858b9c1c7e0153e6873f0774d2
Romain Boyrie <romain@boyrie.email>
2022-04-22 15:21:06 +0200

    Holy Graal

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class DefaultConfig:
     PORT = 3978
     APP_ID = "e27587f8-332e-4207-87eb-94ac5a02cbdb"
     APP_PASSWORD = "lbRM2Nh21rDlQ7SoC6fyJLrPpZfqxbhsga4noW93b5XQim9ybnbRePy1Z410"
-    LUIS_KEY = os.environ.get("MicrosoftSubscribeId", "Environment variable does not exist")
-    LUIS_ID = os.environ.get("MicrosoftLUISId", "Environment variable does not exist")
-    LUIS_ENDPOINT = os.environ.get("MicrosoftLUISEndpoint", "Environment variable does not exist")
+    LUIS_KEY = "708b8499902a49d6902bdfae011bca08"
+    LUIS_ID = "c5a9c03c-654b-43cc-b113-f2a80a849554"
+    LUIS_ENDPOINT = "https://sofistica.cognitiveservices.azure.com/"
 
