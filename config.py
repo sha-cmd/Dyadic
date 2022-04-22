9b88dbefcb2e1094186b277bec8bf5c9399c8530
Romain Boyrie <romain@boyrie.email>
2022-04-22 15:45:30 +0200

    Identity under versioning

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = "e27587f8-332e-4207-87eb-94ac5a02cbdb"
-    APP_PASSWORD = "lbRM2Nh21rDlQ7SoC6fyJLrPpZfqxbhsga4noW93b5XQim9ybnbRePy1Z410"
+    APP_ID = "8a173d35-e05f-45f7-a982-3da8307c7514"
+    APP_PASSWORD = "b.e8Q~hoxcaSndsqxaTVOVc4y-M5YpeglFXbPa0X"
     LUIS_KEY = "708b8499902a49d6902bdfae011bca08"
     LUIS_ID = "c5a9c03c-654b-43cc-b113-f2a80a849554"
     LUIS_ENDPOINT = "https://sofistica.cognitiveservices.azure.com/"
