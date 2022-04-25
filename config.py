c6ae43e2338eb82e1c450838ba1253b6a564d613
Romain Boyrie <romain@boyrie.email>
2022-04-25 14:53:22 +0200

    Bot Shift

--- config.py
+++ config.py
@@ -1,14 +1,21 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
+"""Configuration for the bot."""
+
+import os
+
 
 class DefaultConfig:
-    """ Bot Configuration """
+    """Configuration for the bot."""
 
     PORT = 3978
-    APP_ID = "e27587f8-332e-4207-87eb-94ac5a02cbdb"
-    APP_PASSWORD = "b.e8Q~hoxcaSndsqxaTVOVc4y-M5YpeglFXbPa0X"
-    LUIS_KEY = "708b8499902a49d6902bdfae011bca08"
-    LUIS_ID = "c5a9c03c-654b-43cc-b113-f2a80a849554"
-    LUIS_ENDPOINT = "https://sofistica.cognitiveservices.azure.com/"
-
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    LUIS_APP_ID = os.environ.get("LuisAppId", "")
+    LUIS_API_KEY = os.environ.get("LuisAPIKey", "")
+    # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
+    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "")
+    APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
+        "AppInsightsInstrumentationKey", ""
+    )
