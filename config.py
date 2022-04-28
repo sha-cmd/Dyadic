8b9ee0712dc2273f40936e1ef46466121e0ce63f
Romain Boyrie <romain@boyrie.email>
2022-04-28 14:48:05 +0200

    Listening as it is taught

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ import os
 class DefaultConfig:
     """Configuration for the bot."""
 
-    PORT = 443  # 3978
+    PORT = 3978
     APP_ID = "e27587f8-332e-4207-87eb-94ac5a02cbdb"
     APP_PASSWORD = "b.e8Q~hoxcaSndsqxaTVOVc4y-M5YpeglFXbPa0X"
     LUIS_APP_ID = "c5a9c03c-654b-43cc-b113-f2a80a849554"
