30925cf597b78a8bd1a10f09b2984d502d3dd450
Romain Boyrie <romain@boyrie.email>
2022-04-28 14:57:44 +0200

    Listening as it is taught

--- app.py
+++ app.py
@@ -89,8 +89,13 @@ async def messages(req: Request) -> Response:
 APP = web.Application(middlewares=[bot_telemetry_middleware, aiohttp_error_middleware])
 APP.router.add_post("/api/messages", messages)
 
-if __name__ == "__main__":
+
+def main():
     try:
-        web.run_app(APP, host="localhost", port=CONFIG.PORT)
+        web.run_app(APP, host="0.0.0.0", port=CONFIG.PORT)
     except Exception as error:
         raise error
+
+
+if __name__ == "__main__":
+    main()
