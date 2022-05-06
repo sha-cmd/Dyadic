c7eaa7b3984260e9ee16c678e09254e222b5d4eb
Romain Boyrie <romain@boyrie.email>
2022-05-06 09:16:33 +0200

    Connexion to instance To Do : hide that with github secrets

--- config.py
+++ config.py
@@ -12,10 +12,10 @@ class DefaultConfig:
     PORT = 8000
     APP_ID = "cf9f2854-e02d-4158-9689-3cc7f023388c"
     APP_PASSWORD = "tona@b1Utw3)[H-wRJ|uh1-CE"
-    LUIS_APP_ID = "e919dd6c-f4f9-4282-ae8c-d9d944d79940"
-    LUIS_API_KEY = "0656ff5a0d2c4a28954eedb0cd696c23"
+    LUIS_APP_ID = "b60bdae3-2c61-4641-8e67-555460e637dc"
+    LUIS_API_KEY = "41d9afa00a284c538575ad4926339639"
     # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
     LUIS_API_HOST_NAME = "westeurope.api.cognitive.microsoft.com"
-    LUIS_AUTH_KEY = "0e20947b94844978bbf151bf90243ff0"  # Authoring Key
-    LUIS_AUTH_ENDPOINT = "https://sidney.cognitiveservices.azure.com/"  # Authoring Endpoint
+    LUIS_AUTH_KEY = "c343d2bd55a3499b8f345894394d70b5"  # Authoring Key
+    LUIS_AUTH_ENDPOINT = "https://silicon.cognitiveservices.azure.com/"  # Authoring Endpoint
     APPINSIGHTS_INSTRUMENTATION_KEY = "ec426a1e-af3f-4391-9d24-d3abdd7347d6"
