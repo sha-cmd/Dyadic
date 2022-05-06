bb1dcc40b895b0c25ef075ebf61671a71f20ad24
Romain Boyrie <romain@boyrie.email>
2022-05-06 15:45:24 +0200

    Fix bug for tests

--- tests/test_axioms.py
+++ tests/test_axioms.py
@@ -1,6 +1,8 @@
 """Fichier de tests utilisant une requête et une prédiction. La validation de cette prédiction est effectué
 par l’assertion d’une sortie vers un dictionnaire de valeurs pré-établi par l’évaluateur"""
 
+import os
+
 from azure.cognitiveservices.language.luis.runtime import LUISRuntimeClient
 from msrest.authentication import CognitiveServicesCredentials
 from aiounittest.case import AsyncTestCase
@@ -11,17 +13,17 @@ class LuisTest(AsyncTestCase):
     Le fichier pytest.ini permet de configurer notamment la gestion de la réponse asynchrone."""
 
     async def test_query_luis(self):
-        app_id = "b60bdae3-2c61-4641-8e67-555460e637dc"
-        predictionKey = "41d9afa00a284c538575ad4926339639"
-        predictionEndpoint = "https://westeurope.api.cognitive.microsoft.com/"
+        app_id = os.environ['LUIS_APP_ID']
+        predictionKey = os.environ['LUIS_API_KEY']
+        predictionEndpoint = "https://" + os.environ['LUIS_API_HOST_NAME']
         predictionRequest = {
             "query": "I want a flight from Paris to London, on 22th may 2022"
                      " and return on 25th may 2022, for an amount of 75 dollars."}
-        ent_dict = {"75": "budget", "london": ["dst_city", "builtin.geographyV2.city"],
+        ent_dict = {"75": ["budget", "builtin.number"], "london": ["dst_city", "builtin.geographyV2.city"],
                     "paris": ["or_city", "builtin.geographyV2.city"],
                     "22th may 2022": "builtin.datetimeV2.date",
                     "25th may 2022": "builtin.datetimeV2.date",
-                    "2022": "builtin.number", "2022": "builtin.number", "75": "builtin.number"}
+                    "2022": "builtin.number", "2022": "builtin.number"}
         runtimeCredentials = CognitiveServicesCredentials(predictionKey)
         clientRuntime = LUISRuntimeClient(endpoint=predictionEndpoint, credentials=runtimeCredentials)
         predictionResponse = clientRuntime.prediction.resolve(app_id, query=predictionRequest)
