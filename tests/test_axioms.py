da79bf07e94ad352a60f4bf7d305d44901c152ff
Romain Boyrie <romain@boyrie.email>
2022-05-06 09:15:18 +0200

    Tests with dictionary of control

--- tests/test_axioms.py
+++ tests/test_axioms.py
@@ -1,20 +1,27 @@
 from azure.cognitiveservices.language.luis.runtime import LUISRuntimeClient
 from msrest.authentication import CognitiveServicesCredentials
+from aiounittest.case import AsyncTestCase
 
 
-
-def query_luis_test():
-    app_id = "cf9f2854-e02d-4158-9689-3cc7f023388c"
-    predictionKey = "0656ff5a0d2c4a28954eedb0cd696c23"
-    predictionEndpoint = "https://westeurope.api.cognitive.microsoft.com/"
-    predictionRequest = {"query": "I want a flight from Paris to London, on 22th may 2022 and return on 25th may 2022, for an amount of 75 dollars."}
-    typ_list = ["budget", "dst_city", "or_city", "builtin.datetimeV2.date", "builtin.datetimeV2.date",
-                "builtin.geographyV2.city", "builtin.geographyV2.city", "builtin.number", "builtin.number", "builtin.number"]
-    ent_list = ["75", "london ,", "paris", "22th may 2022", "25th may 2022",
-                "paris", "london", "2022", "2022", "75"]
-    runtimeCredentials = CognitiveServicesCredentials(predictionKey)
-    clientRuntime = LUISRuntimeClient(endpoint=predictionEndpoint, credentials=runtimeCredentials)
-    predictionResponse = clientRuntime.prediction.resolve(app_id, query=predictionRequest)
-    for x in range(len(predictionResponse.entities)):
-        assert typ_list[x] == str(predictionResponse.entities[x].type)
-        assert ent_list[x] == predictionResponse.entities[x].entity
+class LuisTest(AsyncTestCase):
+    async def test_query_luis(self):
+        app_id = "b60bdae3-2c61-4641-8e67-555460e637dc"
+        predictionKey = "41d9afa00a284c538575ad4926339639"
+        predictionEndpoint = "https://westeurope.api.cognitive.microsoft.com/"
+        predictionRequest = {
+            "query": "I want a flight from Paris to London, on 22th may 2022"
+                     " and return on 25th may 2022, for an amount of 75 dollars."}
+        ent_dict = {"75": "budget", "london": ["dst_city", "builtin.geographyV2.city"],
+                    "paris": ["or_city", "builtin.geographyV2.city"],
+                    "22th may 2022": "builtin.datetimeV2.date",
+                    "25th may 2022": "builtin.datetimeV2.date",
+                    "2022": "builtin.number", "2022": "builtin.number", "75": "builtin.number"}
+        runtimeCredentials = CognitiveServicesCredentials(predictionKey)
+        clientRuntime = LUISRuntimeClient(endpoint=predictionEndpoint, credentials=runtimeCredentials)
+        predictionResponse = clientRuntime.prediction.resolve(app_id, query=predictionRequest)
+        for x in range(len(predictionResponse.entities)):
+            entity = ent_dict[predictionResponse.entities[x].entity.replace(',', '').rstrip(' ')]
+            if not type(entity) == list:
+                assert predictionResponse.entities[x].type == entity
+            elif type(entity) == list:
+                assert predictionResponse.entities[x].type in entity
