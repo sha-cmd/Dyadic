61e9413b5c101ba17efe18f27984d3a614ef5664
Romain Boyrie <romain@boyrie.email>
2022-05-06 15:46:12 +0200

    Accomplish a pretty good level of security

--- config.py
+++ config.py
@@ -10,12 +10,12 @@ class DefaultConfig:
     """Configuration for the bot."""
 
     PORT = 8000
-    APP_ID = "cf9f2854-e02d-4158-9689-3cc7f023388c"
-    APP_PASSWORD = "tona@b1Utw3)[H-wRJ|uh1-CE"
-    LUIS_APP_ID = "b60bdae3-2c61-4641-8e67-555460e637dc"
-    LUIS_API_KEY = "41d9afa00a284c538575ad4926339639"
+    APP_ID = os.environ['APP_ID']
+    APP_PASSWORD = os.environ['APP_PASSWORD']
+    LUIS_APP_ID = os.environ['LUIS_APP_ID']
+    LUIS_API_KEY = os.environ['LUIS_API_KEY']
     # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
-    LUIS_API_HOST_NAME = "westeurope.api.cognitive.microsoft.com"
-    LUIS_AUTH_KEY = "c343d2bd55a3499b8f345894394d70b5"  # Authoring Key
-    LUIS_AUTH_ENDPOINT = "https://silicon.cognitiveservices.azure.com/"  # Authoring Endpoint
-    APPINSIGHTS_INSTRUMENTATION_KEY = "ec426a1e-af3f-4391-9d24-d3abdd7347d6"
+    LUIS_API_HOST_NAME = os.environ['LUIS_API_HOST_NAME']
+    LUIS_AUTH_KEY = os.environ['LUIS_AUTH_KEY']  # Authoring Key
+    LUIS_AUTH_ENDPOINT = os.environ['LUIS_AUTH_ENDPOINT']  # Authoring Endpoint
+    APPINSIGHTS_INSTRUMENTATION_KEY = os.environ['APPINSIGHTS_INSTRUMENTATION_KEY']
