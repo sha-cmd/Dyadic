42d9d8055babf13f66271a4de57f48e5f64fcec2
Romain Boyrie <romain@boyrie.email>
2022-05-10 09:33:42 +0200

    Provide all skill to state management.

--- bots/state_management_bot.py
+++ bots/state_management_bot.py
@@ -1,6 +1,9 @@
 from botbuilder.core import ActivityHandler
 from botbuilder.core import ConversationState
 from botbuilder.core import UserState
+from botbuilder.core import TurnContext
+from user_profile import UserProfile
+from conversation_data import ConversationData
 
 
 class StateManagementBot(ActivityHandler):
@@ -16,3 +19,57 @@ class StateManagementBot(ActivityHandler):
 
         self.conversation_state = conversation_state
         self.user_state = user_state
+        self.conversation_data_accessor = self.conversation_state.create_property(
+            "ConversationData"
+        )
+        self.user_profile_accessor = self.user_state.create_property("UserProfile")
+
+    async def on_message_activity(self, turn_context: TurnContext):
+        # Get the state properties from the turn context.
+        user_profile = await self.user_profile_accessor.get(turn_context, UserProfile)
+        conversation_data = await self.conversation_data_accessor.get(
+            turn_context, ConversationData
+        )
+
+        if user_profile.name is None:
+            # First time around this is undefined, so we will prompt user for name.
+            if conversation_data.prompted_for_user_name:
+                # Set the name to what the user provided.
+                user_profile.name = turn_context.activity.text
+
+                # Acknowledge that we got their name.
+                await turn_context.send_activity(
+                    f"Thanks {user_profile.name}. To see conversation data, type anything."
+                )
+
+                # Reset the flag to allow the bot to go though the cycle again.
+                conversation_data.prompted_for_user_name = False
+            else:
+                # Prompt the user for their name.
+                await turn_context.send_activity("What is your name?")
+
+                # Set the flag to true, so we don't prompt in the next turn.
+                conversation_data.prompted_for_user_name = True
+        else:
+            # Add message details to the conversation data.
+            conversation_data.timestamp = self.__datetime_from_utc_to_local(
+                turn_context.activity.timestamp
+            )
+            conversation_data.channel_id = turn_context.activity.channel_id
+
+            # Display state data.
+            await turn_context.send_activity(
+                f"{user_profile.name} sent: {turn_context.activity.text}"
+            )
+            await turn_context.send_activity(
+                f"Message received at: {conversation_data.timestamp}"
+            )
+            await turn_context.send_activity(
+                f"Message received from: {conversation_data.channel_id}"
+            )
+
+    async def on_turn(self, turn_context: TurnContext):
+        await super().on_turn(turn_context)
+
+        await self.conversation_state.save_changes(turn_context)
+        await self.user_state.save_changes(turn_context)
