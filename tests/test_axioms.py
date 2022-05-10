a68dea76bcc0f7cdc98ee6d11150705e5bd76840
Romain Boyrie <romain@boyrie.email>
2022-05-10 11:02:10 +0200

    Passage des paramètres Réécriture complète des tests.

--- tests/test_axioms.py
+++ tests/test_axioms.py
@@ -8,28 +8,24 @@ from msrest.authentication import CognitiveServicesCredentials
 from aiounittest.case import AsyncTestCase
 
 
-class LuisTest(AsyncTestCase):
-    """Fonction utilisant le format de tests générable par la commande pytest tests/ à la racine du projet.
-    Le fichier pytest.ini permet de configurer notamment la gestion de la réponse asynchrone."""
-
-    async def test_query_luis(self):
-        app_id = os.environ['LUIS_APP_ID']
-        predictionKey = os.environ['LUIS_API_KEY']
-        predictionEndpoint = "https://" + os.environ['LUIS_API_HOST_NAME']
-        predictionRequest = {
-            "query": "I want a flight from Paris to London, on 22th may 2022"
-                     " and return on 25th may 2022, for an amount of 75 dollars."}
-        ent_dict = {"75": ["budget", "builtin.number"], "london": ["dst_city", "builtin.geographyV2.city"],
-                    "paris": ["or_city", "builtin.geographyV2.city"],
-                    "22th may 2022": "builtin.datetimeV2.date",
-                    "25th may 2022": "builtin.datetimeV2.date",
-                    "2022": "builtin.number", "2022": "builtin.number"}
-        runtimeCredentials = CognitiveServicesCredentials(predictionKey)
-        clientRuntime = LUISRuntimeClient(endpoint=predictionEndpoint, credentials=runtimeCredentials)
-        predictionResponse = clientRuntime.prediction.resolve(app_id, query=predictionRequest)
-        for x in range(len(predictionResponse.entities)):
-            entity = ent_dict[predictionResponse.entities[x].entity.replace(',', '').rstrip(' ')]  # Luis capture "london ," as "london
-            if not type(entity) == list:
-                assert predictionResponse.entities[x].type == entity
-            elif type(entity) == list:
-                assert predictionResponse.entities[x].type in entity
+async def query_luis_test(luisapiid, luisapikey, luisapihostname):
+    app_id = luisapiid
+    predictionKey = luisapikey
+    predictionEndpoint = "https://" + luisapihostname
+    predictionRequest = {
+        "query": "I want a flight from Paris to London, on 22th may 2022"
+                 " and return on 25th may 2022, for an amount of 75 dollars."}
+    ent_dict = {"75": ["budget", "builtin.number"], "london": ["dst_city", "builtin.geographyV2.city"],
+                "paris": ["or_city", "builtin.geographyV2.city"],
+                "22th may 2022": "builtin.datetimeV2.date",
+                "25th may 2022": "builtin.datetimeV2.date",
+                "2022": "builtin.number", "2022": "builtin.number"}
+    runtimeCredentials = CognitiveServicesCredentials(predictionKey)
+    clientRuntime = LUISRuntimeClient(endpoint=predictionEndpoint, credentials=runtimeCredentials)
+    predictionResponse = clientRuntime.prediction.resolve(app_id, query=predictionRequest)
+    for x in range(len(predictionResponse.entities)):
+        entity = ent_dict[predictionResponse.entities[x].entity.replace(',', '').rstrip(' ')]  # Luis capture "london ," as "london
+        if not type(entity) == list:
+            assert predictionResponse.entities[x].type == entity
+        elif type(entity) == list:
+            assert predictionResponse.entities[x].type in entity
