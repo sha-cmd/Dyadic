563a84f8cd43a0029a79d3128efa43743857d98a
Romain Boyrie <romain@boyrie.email>
2022-05-11 10:00:34 +0200

    Small changes of host port address

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ def init_func(argv=None):
 if __name__ == "__main__":
     APP = init_func()
     try:
-        web.run_app(APP, host="localhost", port=CONFIG.PORT)
+        web.run_app(APP, host="0.0.0.0", port=CONFIG.PORT)
     except Exception as error:
         raise error
 
